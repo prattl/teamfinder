8c817668990e8c160a7277d241b5cbc0d386fa7e
Lenny Pratt <lenny@prattdev.net>
2017-02-20 18:21:39 -0500

    Move deployment env to home directory and update uwsgi and deploy scripts

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -8,12 +8,13 @@ fi
 COMMIT=$1
 BASE_DIR=/home/ubuntu/teamfinder
 API_DIR=${BASE_DIR}/api
-VENV_DIR=${API_DIR}/env
+VENV_DIR=/home/ubuntu/env
 CLIENT_DIR=${BASE_DIR}/client
 
 cd ${API_DIR}
 git fetch
 git checkout --force ${COMMIT}
+ln -s /home/ubuntu/settings_local.py
 ${VENV_DIR}/bin/python manage.py migrate --noinput
 ${VENV_DIR}/bin/python manage.py collectstatic --noinput
 cd ${CLIENT_DIR}
