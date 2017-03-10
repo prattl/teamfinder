9e0b19973f134f99b1fb50f58c36b5f3ca6afc07
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-03-10 14:26:50 -0500

    Create HoC for retrieving a team from params

--- client/src/components/connectors/WithTeam.jsx
+++ client/src/components/connectors/WithTeam.jsx
@@ -1,28 +1,34 @@
-// import React, { Component } from 'react'
-// import { connect } from 'react-redux'
-// import { createStructuredSelector } from 'reselect'
-//
-// import { requestAllFixturesIfNeeded } from 'actions/fixtures'
-// import { teamsSelector } from 'utils/selectors'
-//
-// const withTeam = (WrappedComponent) => {
-//     class WithTeam extends Component {
-//
-//         componentDidMount() {
-//             this.props.onLoad()
-//         }
-//
-//         render() {
-//             return <WrappedComponent {...this.props} />
-//         }
-//
-//     }
-//     WithTeam = connect(teamsSelector, { onLoad: requestAllFixturesIfNeeded })(WithFixtures)
-//     return WithFixtures
-// }
-//
-// export const withAllFixtures = withFixtures(
-//     createStructuredSelector({
-//         fixtures: fixturesSelector,
-//     }),
-// )
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { requestTeam } from 'actions/teams'
+import { teamsSelector } from 'utils/selectors'
+
+export const withTeam = teamId => (WrappedComponent) => {
+
+    class WithTeam extends Component {
+
+        componentDidMount() {
+            this.props.onLoad(teamId)
+        }
+
+        render() {
+            const { teams } = this.props
+            const team = teams[teamId] || {}
+
+            return <WrappedComponent team={team} {...this.props} />
+        }
+
+    }
+    WithTeam = connect(
+        teamsSelector,
+        { onLoad: requestTeam }
+    )(WithTeam)
+    return WithTeam
+}
+
+export const withTeamFromParams = WrappedComponent => props => {
+    const ConnectedComponent = withTeam(props.params.id)(WrappedComponent)
+    return <ConnectedComponent />
+}
--- client/src/containers/teams/ManageTeam.jsx
+++ client/src/containers/teams/ManageTeam.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { requestTeam } from 'actions/teams'
@@ -7,6 +7,7 @@ import { Link } from 'react-router'
 import { Label } from 'react-bootstrap'
 import { Col, Row } from 'react-bootstrap'
 import { withAllFixtures } from 'components/connectors/WithFixtures'
+import { withTeam, withTeamFromParams } from 'components/connectors/WithTeam'
 import { requestPlayer } from 'actions/playerSearch'
 import { playerSearchSelector } from 'utils/selectors'
 import { FixtureDisplay, Loading } from 'utils'
@@ -14,57 +15,29 @@ import { CaptainIcon, RegionIcon, PlayersIcon, PositionIcon, SkillBracketIcon } 
 
 class ManageTeam extends Component {
 
-    componentDidMount() {
-        this.props.dispatch(requestTeam(this.props.params.id))
+    static propTypes = {
+        team: PropTypes.object.isRequired
     }
 
     render() {
-        const { params: { id }, teams: { teams } } = this.props
-
-        // const id = this.props.params.id
-        // const { teams } = this.props.teams
-        // if teams[id] is undefined or empty
-        const team = teams[id] || {}
-        const isLoading = team.isLoading
-        const lastUpdated = team.lastUpdated
-
-        // or ...
-        // const teams = this.props.teams
-        // const team = teams.team
-        // const isLoading = teams.isLoading
-        // const lastUpdated = teams.lastUpdated
-
-        // const { fixtures: { regions, positions, skillBrackets } } = this.props
-
-        console.log('TEAM: ', team)
+        const { team: { team, isLoading, lastUpdated } } = this.props
 
         return (
             <div>
-                <h1>Manage Team:</h1>
+                {isLoading ? <Loading /> : (
+                    lastUpdated ? (
+                        <div>
+                            <h1>Manage Team: {team.name}</h1>
+                        </div>
+                    ) : (
+                        <div>Error retrieving team.</div>
+                    )
+                )}
             </div>
         )
     }
 }
 
-// higher order component function that takes in a component
-ManageTeam = connect(
-    // 1
-    // state -> jsobject(json/pythondict/etc...)
-    // => : function
-    // {object notation}
-    state => ({
-        // : : ...
-        teams : state.teams
-    })
-
-    // 2
-    // function(state) {
-    //     return ({
-    //         teams : state.teams
-    //     })
-    // }
-)(ManageTeam)
-
-// ManageTeam = with(ManageTeam)
+ManageTeam = withTeamFromParams(ManageTeam)
 
 export default ManageTeam
