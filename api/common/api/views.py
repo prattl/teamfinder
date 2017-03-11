8151d6cef2a84959785320b69b1cde1e60763899
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-03-11 17:13:12 -0500

    Return 400 if player tries to delete themselves from a team if they are the captain

--- api/common/api/views.py
+++ api/common/api/views.py
@@ -143,9 +143,10 @@ class MembershipViewSet(mixins.RetrieveModelMixin,
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
 
-        if instance.player == self.request.user.player.id and instance.team.captain == self.request.user.player:
+        if instance.player.id == self.request.user.player.id and instance.team.captain.id == self.request.user.player.id:
             return Response({'error': 'You cannot remove yourself from the team if you are the captain.'},
                             status=status.HTTP_400_BAD_REQUEST)
+        return super(MembershipViewSet, self).destroy(request, *args, **kwargs)
 
     def perform_destroy(self, instance):
         # TODO: Don't allow delete of own player
