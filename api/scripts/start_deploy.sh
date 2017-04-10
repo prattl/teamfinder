26917bb413853d1034e488dfbd2da6981567133d
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-04-09 23:09:30 -0400

    Update deploy url to https

--- api/scripts/start_deploy.sh
+++ api/scripts/start_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -d "DEPLOY_SECRET_KEY=${DEPLOY_SECRET_KEY}&COMMIT=${TRAVIS_COMMIT}&BRANCH=${TRAVIS_BRANCH}" dotateamfinder.com:8000/deploy/
+curl -d "DEPLOY_SECRET_KEY=${DEPLOY_SECRET_KEY}&COMMIT=${TRAVIS_COMMIT}&BRANCH=${TRAVIS_BRANCH}" https://dotateamfinder.com:8000/deploy/
