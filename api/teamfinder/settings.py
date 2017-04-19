14983f228f48f89ff25c0ee16b77f2cb74d27296
Lenny Pratt <lenny@prattdev.net>
2017-04-19 07:49:15 -0400

    Add social auth settings

--- api/teamfinder/settings.py
+++ api/teamfinder/settings.py
@@ -180,4 +180,21 @@ EMAIL_PORT = 587
 
 SITE_ID = 1
 
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'teamfinder.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+SOCIAL_AUTH_STEAM_EXTRA_DATA = ['player_data']
+SOCIAL_AUTH_CLEAN_USERNAMES = False
+SOCIAL_AUTH_SANITIZE_REDIRECTS = False
+SOCIAL_AUTH_PROTECTED_FIELDS = ('id', 'pk', 'email', )
+
 from .settings_local import *
