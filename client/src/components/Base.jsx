0c3b63d695e083d00e0edbc0fbb9abd84e956d99
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-02-01 00:04:07 -0500

    Display survey alert

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,14 +1,72 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 
-import Alert from 'react-s-alert'
-import { Grid } from 'react-bootstrap'
+import AlertSystem from 'react-s-alert'
+import {
+    Alert,
+    Button,
+    Col,
+    Grid,
+    Row,
+} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import TopNav from 'components/layout/TopNav'
 import Footer from 'components/layout/Footer'
 import FeedbackButton from 'components/feedback/FeedbackButton'
 import FeedbackModal from 'components/feedback/FeedbackModal'
 
+const surveyLink = 'https://docs.google.com/forms/d/e/1FAIpQLSeIvtZYRnAiUo0Cq5jHK_z_RD53apj7VW-TaboFAUn0TTgzrg/viewform'
+
+class SurveyAlert extends Component {
+    storageKey = 'hideSurveyAlert'
+    storageValue = '1'
+
+    state = {
+        show: false,
+    }
+
+    handleDismiss = () => {
+        localStorage.setItem(this.storageKey, this.storageValue)
+        this.setState({ show: false })
+    }
+
+    componentDidMount() {
+        const hideSurveyAlert = localStorage.getItem(this.storageKey) === this.storageValue
+        if (!hideSurveyAlert) {
+            this.setState({ show: true })
+        }
+    }
+
+    render() {
+        const { show } = this.state
+        return show && (
+            <Row>
+                <Col md={8} mdOffset={2}>
+                    <Alert onDismiss={this.handleDismiss} bsStyle='info' style={{ marginTop: '2rem' }}>
+                        <h4>We need your help!</h4>
+                        <p>
+                            We are planning out some big changes and would love to get your feedback. We've
+                            created a survey for amatuer Dota 2 players. It would be a huge help if you could fill it
+                            out. Thanks!
+                        </p>
+                        <p>
+                            <Button bsStyle='info' href={surveyLink}>Click here to take survey</Button>
+                            <Button bsStyle='link' onClick={this.handleDismiss}>
+                                <span className='text-info'>
+                                    Hide
+                                </span>
+                            </Button>
+                        </p>
+                    </Alert>
+                </Col>
+            </Row>
+        )
+    }
+}
+
 class Base extends Component {
+
+
 
     render() {
         return (
@@ -22,12 +80,14 @@ class Base extends Component {
 
                 <div id='page-content'>
                     <Grid>
+                        <SurveyAlert />
+
                         {this.props.children}
                     </Grid>
                 </div>
 
                 <Footer />
-                <Alert stack={{limit: 3}} effect='slide' />
+                <AlertSystem stack={{limit: 3}} effect='slide' />
                 <FeedbackButton />
                 <FeedbackModal />
             </div>
