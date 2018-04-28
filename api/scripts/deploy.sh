bae0f4349ac02727b31f81aee8e9ea03c938c91f
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-04-28 16:21:36 -0400

    Setup Docker config

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -15,8 +15,8 @@ cd ${API_DIR}
 git fetch
 git checkout --force ${COMMIT}
 ln -sf /home/ubuntu/settings_local.py
-# yes w | ${VENV_DIR}/bin/pip install -r requirements.txt
+${VENV_DIR}/bin/pip install --noinput -r ../requirements.txt
 ${VENV_DIR}/bin/python manage.py migrate --noinput
 ${VENV_DIR}/bin/python manage.py collectstatic --noinput
 cd ${CLIENT_DIR}
-npm run build
+yarn run build
