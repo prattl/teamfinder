24dba96be4a9202fdac5fa779c52789ec6cd13c4
Lenny Pratt <lenny@prattdev.net>
2018-10-07 14:33:40 -0400

    Improve subprocess call during deployment

--- api/common/views.py
+++ api/common/views.py
@@ -16,7 +16,8 @@ def deploy(request):
     if deploy_secret_key != settings.SECRET_KEY:
         return HttpResponseBadRequest('Incorrect key.')
 
-    subprocess.Popen(['scripts/deploy.sh', commit], stdout=subprocess.PIPE)
+    subprocess.run(['scripts/deploy.sh', commit],
+                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return JsonResponse({'result': 'deploy started'})
 
 
