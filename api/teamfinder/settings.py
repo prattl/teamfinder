87e696f7910c12a70b329c07feaf4cbf383a4f1a
Lenny Pratt <lenny@prattdev.net>
2018-10-14 19:40:14 -0400

    Randomize test database name

--- api/teamfinder/settings.py
+++ api/teamfinder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from uuid import uuid4
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +117,9 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
+        'TEST': {
+            'NAME': 'teamfinder_test_{}'.format(uuid4())
+        }
     }
 }
 
